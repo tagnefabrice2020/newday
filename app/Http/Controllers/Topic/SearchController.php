c95c551161321430aa9b317f7576bbf11ee2947b
tagnefabrice2020 <tagnefabrice@gmail.com>
2024-01-26 00:13:21 +0100

    gg

--- app/Http/Controllers/Topic/SearchController.php
+++ app/Http/Controllers/Topic/SearchController.php
@@ -11,28 +11,30 @@ class SearchController extends Controller
     public function search(Request $r)
     {
         $topics = DB::table('topics')
-            ->join('users', 'topics.user_id', '=', 'users.id') 
-            ->select('topics.*') 
-            ->withCount(['questions', 'practiceHistory'])
-            ->where('topics.name', 'like', '%' . $r->search . '%')
-            ->orWhere('topics.description', 'like', '%' . $r->search . '%')
-            ->orWhere('topics.tags', 'like', '%' . $r->search . '%')
-            ->orWhere('users.username', 'like', '%' . $r->search . '%');
+            ->join('users', 'topics.author_id', '=', 'users.id')
+            ->select(
+                'topics.*',
+                DB::raw('(SELECT COUNT(*) FROM questions WHERE questions.topic_id = topics.id) as questions_count'),
+                DB::raw('(SELECT COUNT(*) FROM practice_history WHERE practice_history.topic_id = topics.id) as practice_history_count')
+            )->where(function ($query) use ($r) {
+                $query->where('topics.name', 'like', '%' . $r->search . '%')
+                    ->orWhere('topics.description', 'like', '%' . $r->search . '%')
+                    ->orWhere('topics.tags', 'like', '%' . $r->search . '%')
+                    ->orWhere('users.username', 'like', '%' . $r->search . '%');
+            })
+            ->when($r->has('paid') && $r->paid, function ($query) {
+                $query->where('topics.free', false);
+            })
+            ->when($r->has('type') && $r->type, function ($query) use ($r) {
+                $query->where('topics.type', $r->type);
+            })
+            ->when($r->has('country') && $r->country, function ($query) use ($r) {
+                $query->where('topics.country', $r->country);
+            });
 
-        if ($r->has('payment') && $r->payment) {
-            $topics->where('topics.paid', true);
-        }
 
-        if ($r->has('type') && $r->type) {
-            $topics->where('topics.type', $r->type);
-        }
+        $paginatedTopics = $topics->paginate(10);
 
-        if($r->has('country') && $r->country) {
-            $topics->where('topics.country', $r->country);
-        }
-
-        $topics->paginate(10);
-
-        return response()->json($topics, 200);
+        return response()->json($paginatedTopics, 200);
     }
 }
