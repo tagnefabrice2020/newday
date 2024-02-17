7cb5e1ff646412060c9aaa193e7763d0adea3790
tagnefabrice2020 <71602428+tagnefabrice2020@users.noreply.github.com>
2024-02-17 19:31:02 +0100

    feat: sync user route added

--- app/Http/Controllers/SyncUser/SyncUserController.php
+++ app/Http/Controllers/SyncUser/SyncUserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\SyncUser;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Model\User;
+use App\Models\User;
 
 class SyncUserController extends Controller
 {
