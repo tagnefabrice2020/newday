9c72b90496de1348ad0042d05b704437cff79b4f
tagnefabrice2020 <71602428+tagnefabrice2020@users.noreply.github.com>
2024-02-28 17:09:19 +0100

    feat: clerk auth middleware 1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,8 +45,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Illuminate\Http\Middleware\ClerkAuthenticationMiddleware::class
+            \Illuminate\Routing\Middleware\SubstituteBindings::class
         ],
     ];
 
