a2cb80791178cb0bce60e7c750c9c56b3670878d
tagnefabrice2020 <71602428+tagnefabrice2020@users.noreply.github.com>
2024-02-29 18:12:14 +0100

    clerk auth middleware created

--- app/Http/Middleware/ClerkAuthenticationMiddleware.php
+++ app/Http/Middleware/ClerkAuthenticationMiddleware.php
@@ -1,22 +1,52 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace App\Http\Controllers\SyncUser;
 
-use Closure;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 
-class ClerkAuthenticationMiddleware
+class SyncUserController extends Controller
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        dd($request);
-        return $next($request);
+    public function store (Request $r) {
+        if ($r->type === 'user.created') {
+            $user = new User;
+
+            $user->uuid = Str::orderedUuid();
+            $user->authProviderId = $r->data['id'];
+            $user->name = $r->data['username'] || ($r->data['first_name'].'" "'.$r->data['last_name']);
+            $user->email = $r->data['email_addresses'][0]['email_address'];
+            
+            $user->role_id = 1;
+        
+            $save = $user->save();
+
+            if($save) {
+                return response()->json(['user' => $user]);
+            }
+        } else if ($r->type === 'user.updated') {
+            $user = User::where('email', $r->data['email_addresses'][0]['email_address'])->first();
+
+            if ($r->has("data.username")) {
+                $user->name = $r->data['username'] ;
+            }
+
+            if ($r->has("data.email_addresses")) {
+                $user->email = $r->data['email_addresses'][0]['email_address'];
+            }
+
+            $save = $user->save();
+
+            if($save) {
+                return response()->json(['user' => $user]);
+            }
+
+        }
+    }
+
+    public function test2 () {
+        return Auth::user();
     }
 }
