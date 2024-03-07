7d0f6bc436339b0f7dda0fed284822680040158d
tagnefabrice2020 <71602428+tagnefabrice2020@users.noreply.github.com>
2024-03-07 13:32:47 +0100

    fix: added delete route to api.php

--- app/Http/Controllers/Topic/DeleteTopicController.php
+++ app/Http/Controllers/Topic/DeleteTopicController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Topic;
 use App\Http\Controllers\Controller;
 use App\Models\Topic;
 use Illuminate\Http\Request;
+use Auth;
 
 class DeleteTopicController extends Controller
 {
     public function destroy($id)
     {
-        $topic = Topic::find($id);
+        $topic = Topic::where('uuid', $id)->where("author_id", Auth::id())->first();
 
         if (!$topic) {
             return response()->json(['message' => 'Topic not found'], 404);
